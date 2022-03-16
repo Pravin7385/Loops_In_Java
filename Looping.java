e46256b01840dff97e5f4640109daa6453ebc36a
Pravin7385 <100189071+Pravin7385@users.noreply.github.com>
2022-03-16 15:14:46 +0530

    Add files via upload

---
+++ Looping.java
@@ -0,0 +1,31 @@
+package com.LoopingStatments;
+
+// looping Statements 
+
+public class Looping {
+
+	public static void main(String[] args) {
+
+		// 1. For Loop--->
+		for (int i = 1; i <= 10 ; i++) {
+			System.out.println(i);
+		}
+		
+		// 2.While Loop --->
+		int a=10;
+		while(a<100) {
+			System.out.println("While Loop");
+			break;
+		}
+		
+		// 3.Do-While Loop --->
+		int x=10;
+		do {
+			System.out.println("Do-While Loop");
+		}while(x>100);
+		
+		
+		
+		
+	}
+}
